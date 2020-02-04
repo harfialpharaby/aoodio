768fc7a3c797963f1b89ea64125ab8486c474e94
Harfi <harfialpharaby@gmail.com>
2020-02-04 07:22:59 +0700

    show stars for rate and heart for loved  Changes to be committed: - add fontawesome package 	modified:   package.json 	modified:   public/index.html 	modified:   src/components/AlbumList.jsx 	modified:   src/index.js

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -3,11 +3,29 @@ import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Spinner from 'react-bootstrap/Spinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
 
 export default function AlbumList(props) {
     const [albums, setAlbums] = useState([]);
+    const [isLoading, setLoading] = useState(false)
+
+    const generateStar = (score) => {
+        let stars = []
+        while (score > 0) {
+            if (score < 1) {
+                stars.push(<FontAwesomeIcon icon={faStarHalf} />)
+            } else {
+                stars.push(<FontAwesomeIcon icon={faStar} />)
+            }
+            score--
+        }
+
+        return stars
+    }
     
     useEffect(() => {
+        setLoading(true)
         fetch(`http://theaudiodb.com/api/v1/json/1/searchalbum.php?s=${props.artist}`)
             .then((res) => {
                 return res.json();
@@ -16,19 +34,21 @@ export default function AlbumList(props) {
                     data.album.map(album => {
                         return {
                             id: album.idAlbum,
-                            thumbnail: album.strAlbumThumb,
+                            thumbnail: album.strAlbumThumb || 'https://bandungumroh.com/sie/assets/no_image.png',
                             albumName: album.strAlbum,
                             artistName: album.strArtist,
                             releaseDate: album.intYearReleased,
                             genre: album.strGenre,
                             label: album.strLabel,
                             description: album.strDescriptionEN,
-                            loved: album.intLoved,
-                            score: album.intScore,
-                            voted: album.intScoreVotes
+                            loved: album.intLoved || 0,
+                            score: album.intScore || 'No Rate',
+                            stars: generateStar(album.intScore),
+                            voted: album.intScoreVotes || 0
                         }
                     })
                 )
+                setLoading(false)
             }).catch((err) => {
                 console.log(err);
             });
@@ -44,7 +64,7 @@ export default function AlbumList(props) {
                 </Col>
             </Row>
             {
-                !albums.length
+                isLoading
                     ? (
                         <Row>
                             <Col className="text-center"><Spinner animation="border" variant="success"/></Col>
@@ -62,13 +82,30 @@ export default function AlbumList(props) {
                                     albums.map(album => {
                                         return (
                                             <Col key={album.id}>
-                                                <Card className="shadow-sm mb-3" style={{ width: '18rem' }}>
-                                                    <Card.Img src={album.thumbnail} variant="top" style={{ width: '18rem' }}/>
-                                                    <Card.Body>
-                                                        <h2>{album.albumName}</h2>
-                                                        <p>{album.genre}</p>
-                                                        <p>{album.label}</p>
-                                                        <p>{album.albumName}</p>
+                                                <Card className="shadow-sm mb-3" style={{ width: '15rem' }}>
+                                                    <Card.Img src={album.thumbnail} variant="top" style={{ width: '15rem' }}/>
+                                                    <Card.Body className="p-0">
+                                                        <h2 className="px-3">{album.albumName}</h2>
+                                                        <div className="font-weight-light px-3" style={{fontSize: '12px'}}>{album.genre}</div>
+                                                        <div className="px-3">{album.label}</div>
+                                                        <div className="d-flex justify-content-between mt-4 mb-3">
+                                                            <span>
+                                                                {
+                                                                    album.stars.length
+                                                                        ? <span className="mr-1 text-warning">{album.stars}</span>
+                                                                        : null
+                                                                }
+                                                                <span className="font-weight-light" style={{fontSize: '12px'}}>
+                                                                    {album.score}
+                                                                </span>
+                                                            </span>
+                                                            <span>
+                                                                <FontAwesomeIcon icon={faHeart} className="mr-1 text-danger"/>
+                                                                <span className="font-weight-light" style={{fontSize: '12px'}}>
+                                                                    {album.loved}
+                                                                </span>
+                                                            </span>
+                                                        </div>
                                                     </Card.Body>
                                                 </Card>
                                             </Col>
